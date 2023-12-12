1b1ae66ea74d078b2c4ce41c4846fd26439b2444
[Tanha] <[julus8238@gmail.com]>
2023-12-12 15:59:30 +0900

    add.SceneChange.231212
    인트로 화면에서 메뉴로 넘어가기

--- Assets/Script/IntroRtanMoving.cs
+++ Assets/Script/IntroRtanMoving.cs
@@ -2,36 +2,35 @@ using UnityEngine;
 
 public class IntroRtanMoving : MonoBehaviour
 {
-    float direction = 0.005f;
-    bool IsMeeting = false;
+    bool isMeeting = false;
     Animator anim;
+    Rigidbody2D rigid;
+    public int nextmove;
 
-    void Start()
+
+    void Awake()
     {
         Application.targetFrameRate = 60;
         anim = GetComponent<Animator>(); // Animator를 초기화
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        if (transform.position.y < -10)
-        {
-            direction = 0;
-        }
-
-        if (transform.position.x > 12)
-        {
-            direction = 0;
-        }
-        transform.position += new Vector3(direction, 0, 0);
+        rigid.velocity = new Vector2 (nextmove, rigid.velocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    {   
         if (collision.gameObject.tag == "Player")
         {
-            // 애니메이션 재생
-            anim.SetBool("Ismeeting", true);
+            isMeeting = true;
+            anim.SetBool("IsMeeting", true);
         }
+    }
+
+    private void Think() 
+    {
+        nextmove = Random.Range(-1, 2);
     }
 }
